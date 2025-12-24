ac4b9f8f67ee5dcabf9857180bd11a827a28a6f5
KhanhDev <kngo23612@gmail.com>
2025-12-24 10:50:52 +0700

    Update imports and handle missing dependencies
    Refactor imports and add error handling for missing packages.

--- tool.py
+++ tool.py
@@ -1,12 +1,99 @@
-import requests
-import time
-import re
-import random
-import string
-import sys
+
+import threading, base64, os, time, re, json, random, subprocess, string
+
+from datetime import datetime, timedelta
+
+from time import sleep, strftime
+
+from bs4 import BeautifulSoup
+
+import requests, socket, sys
+
+
+
+try:
+
+    from faker import Faker
+
+    from requests import session
+
+    from colorama import Fore, Style
+
+    from random import randint
+
+    import pystyle
+
+    import socks
+
+except:
+
+    os.system("pip install faker")
+
+    os.system("pip install requests")
+
+    os.system("pip install colorama")
+
+    os.system("pip install bs4")
+
+    os.system("pip install pystyle")
+
+    print('__Vui Lòng Chạy Lại Tool__')
+
+    exit()
 
 BASE_URL = "https://api.mail.tm"
 
+from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
+
+
+
+# Màu sắc
+
+xnhac = "\033[1;36m"
+
+do = "\033[1;31m"
+
+luc = "\033[1;32m"
+
+vang = "\033[1;33m"
+
+xduong = "\033[1;34m"
+
+hong = "\033[1;35m"
+
+trang = "\033[1;39m"
+
+whiteb = "\033[1;39m"
+
+red = "\033[0;31m"
+
+redb = "\033[1;31m"
+
+end = '\033[0m'
+
+dev = "\033[1;39m[\033[1;31m\033[1;39m]\033[1;39m"
+
+def banner():
+
+    banner = f"""
+\033[1;32m ████████╗   ██╗ ██ ██╗   ██╗ █████╗ ███╗   ██╗██╗  ██╗
+\033[1;32m ╚══██╔══╝   ██║██╔╝██║   ██║██   ██╗████╗  ██║██║  ██║
+\033[1;32m    ██║      ████╔╝ ████████║███████║██╔██╗ ██║███████║
+\033[1;32m    ██║      ██╔██╗ ██║   ██║██   ██║██║╚██╗██║██╔══██║
+\033[1;32m    ██║      ██║╚██╗██║   ██║██   ██║██║  ████║██║  ██║
+\033[1;32m    ╚═╝  ██  ╚═╝ ╚═╝╚═╝   ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝
+\033[97m╔═════════════════════════════════════════════════╗
+\033[1;97m║ Tool By: \033[1;32mShop T.Khanh            \033[1;97mPhiên Bản: \033[1;32m1.0 \033[97m║    
+\033[97m╚═════════════════════════════════════════════════╝
+
+\033[97m╔═══════════════════════════════════════════════════════╗
+\033[1;97m║[\033[1;91m❣\033[1;97m]\033[1;97m Zalo\033[1;31m  : \033[1;97m☞ \033[1;36mhttp://zalo.me/0854533557\033[1;31m♔ \033[1;97m☜             ║
+\033[1;97m║[\033[1;91m❣\033[1;97m]\033[1;97m Tiktok\033[1;31m  : \033[1;97m☞ \033[1;36m@tk_a_h\033[1;31m♔ \033[1;97m☜                         ║
+\033[1;97m║[\033[1;91m❣\033[1;97m]\033[1;97m Facebook\033[1;31m : \033[1;97m☞ \033[1;36mhttps://www.facebook.com/tkhanh223\033[1;31m♔ \033[1;97m☜ ║
+\033[97m╚═══════════════════════════════════════════════════════╝
+
+"""
+    
 def random_name(length=8):
     return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(length))
 
